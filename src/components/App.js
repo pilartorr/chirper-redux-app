9591e64b79f138c616f418d3bc73c751a5463e30
pilartorr <sauleturme@gmail.com>
2020-04-09 12:33:22 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App text-center">
+        Hello Chirper App
+      </div>
+    );
+  }
+}
+
+export default App;
